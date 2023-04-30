b3943482d6ce716065c1aa2e86946f73f82e0147
Cooper-Pilot <47166588+Cooper-Pilot@users.noreply.github.com>
2023-04-30 17:27:45 -0400

    Add and remove tabs working

--- InterfaceTest/MainWindow.xaml.cs
+++ InterfaceTest/MainWindow.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Diagnostics;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Media;
 
 namespace InterfaceTest
 {
@@ -98,11 +94,48 @@ namespace InterfaceTest
         private void RemoveTab_Click(object sender, RoutedEventArgs e)
         {
             TabItem tabItem = ((sender as MenuItem).Parent as ContextMenu).PlacementTarget as TabItem;
-            if (tabItem.Parent is TabControl tabControl)
+            if (!(tabItem.Parent is TabControl tabControl)) return;
+
+            if (tabControl.Items.Count > 1)
             {
-                if(tabControl.Items.Count > 1)
-                    tabControl.Items.Remove(tabItem);
+                tabControl.Items.Remove(tabItem);
+                return;
             }
+
+            ContentControl contentControl = (tabControl.Parent as DockPanel).Parent as ContentControl;
+            Grid grid = contentControl.Parent as Grid;
+            if (grid.RowDefinitions.Count > 1)
+            {
+                if ((int)contentControl.GetValue(Grid.RowProperty) == 0)
+                    MergeSplit(grid, Grid.RowProperty, 2);
+                else
+                    MergeSplit(grid, Grid.RowProperty, 0);
+            }
+            else if (grid.ColumnDefinitions.Count > 1)
+            {
+                if ((int)contentControl.GetValue(Grid.ColumnProperty) == 0)
+                    MergeSplit(grid, Grid.ColumnProperty, 2);
+                else
+                    MergeSplit(grid, Grid.ColumnProperty, 0);
+            }
+        }
+
+        private void MergeSplit(Grid grid, DependencyProperty gridDirectionProperty, int propertyKeepValue)
+        {
+            UIElement kept = null;
+            foreach (UIElement child in grid.Children)
+            {
+                if ((int)child.GetValue(gridDirectionProperty) == propertyKeepValue)
+                    kept = child;
+            }
+            if (kept == null)
+                return;
+            Grid parentGrid = grid.Parent as Grid;
+            Grid.SetColumn(kept, (int)grid.GetValue(Grid.ColumnProperty));
+            Grid.SetRow(kept, (int)grid.GetValue(Grid.RowProperty));
+            grid.Children.Remove(kept);
+            parentGrid.Children.Remove(grid);
+            parentGrid.Children.Add(kept);
         }
     }
 }
